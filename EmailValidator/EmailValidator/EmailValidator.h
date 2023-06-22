210f24dcb686278668e572e45cf77fece7f7323e
H S Negi <hsnegi@Hs-MacBook-Pro.local>
2023-06-22 19:34:28 +0530

    Add Framework

---
+++ EmailValidator/EmailValidator/EmailValidator.h
@@ -0,0 +1,18 @@
+//
+//  EmailValidator.h
+//  EmailValidator
+//
+//  Created by H S Negi on 22/06/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EmailValidator.
+FOUNDATION_EXPORT double EmailValidatorVersionNumber;
+
+//! Project version string for EmailValidator.
+FOUNDATION_EXPORT const unsigned char EmailValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EmailValidator/PublicHeader.h>
+
+
