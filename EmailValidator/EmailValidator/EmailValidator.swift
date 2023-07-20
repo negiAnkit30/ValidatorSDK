f4ce7d2f6fc57282a3df2e313da3638f7d329fac
Bhaank <>
2023-07-20 17:02:01 +0530

    dkk

--- EmailValidator/EmailValidator/EmailValidator.swift
+++ EmailValidator/EmailValidator/EmailValidator.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public class EmailValidator {
-    
+    //dedkrlfm
    public static func validateEmail(email:String) -> Bool {
         let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9-]+\\.[A-Za-z]{2,6}"
         return NSPredicate(format: "SELF MATCHES %@", emailRegex).evaluate(with: email)
