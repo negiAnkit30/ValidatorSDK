d896bacb29f75c40b58bf6a9c0b07d3e10f43a57
Bhaank <>
2023-07-20 17:04:21 +0530

    nk

--- EmailValidator/EmailValidator/EmailValidator.swift
+++ EmailValidator/EmailValidator/EmailValidator.swift
@@ -13,5 +13,5 @@ public class EmailValidator {
         let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9-]+\\.[A-Za-z]{2,6}"
         return NSPredicate(format: "SELF MATCHES %@", emailRegex).evaluate(with: email)
     }
-    
+ //gvg
 }
